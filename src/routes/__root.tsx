b26e6a65a155687c5479d5973bf5ba16810b9d08
Holmes-EH <s.holmes.eh@gmail.com>
2026-01-18 12:41:20 +0100

    delete Header component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
-import Header from "../components/Header";
-
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
